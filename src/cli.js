157ebceb60558c94ffd3b6144fd2129cc0af40d0
2karina <karina_kap@hotmail.com>
2023-10-09 17:35:39 -0300

    T003 - feat: chalk implementation and corrected imports

--- src/cli.js
+++ src/cli.js
@@ -1,53 +1,31 @@
 #!/usr/bin/env node
 
 const { program } = require('commander');
-const fs = require('fs');
-const readline = require('readline');
-// const fetch = require('node-fetch');
+const readFile = require('./index');
+const chalk = require('chalk');
+const log = console.log;
 
 program.version('1.0.0');
 
 program
-    .command('read-file <file-name>')
-    .description('Read the content of a file')
-    .action((fileName) => {
-        readFile(fileName);
-    });
+  .command('read-file <file-name>')
+  .description('Read the content of a file')
+  .action((fileName) => {
+    readFile(fileName)
+      .then((results) => {
+        results.forEach((result) => {
+          if (result.error) {
+            const formattedResult = `${chalk.red.bold('[Invalid]')}: ${result.url}`;
+            log(formattedResult);
+          } else {
+            const formattedResult = `${chalk.green.bold('[Valid]')}: ${result.url}`;
+           log(formattedResult);
+          }
+        });
+      })
+      .catch((error) => {
+        console.error('An error occurred:', error.message);
+      });
+  });
 
-function readFile(fileName) {
-    const fileStream = fs.createReadStream(fileName, 'utf8');
-    const linkRegex = /https?:\/\/\S+/g;
-    const results = [];
-
-    const rl = readline.createInterface({
-        input: fileStream,
-        crlfDelay: Infinity,
-    });
-    rl.on('line', (line) => {
-        const links = line.match(linkRegex);
-        if (links) {
-            links.forEach((link) => {
-                fetch(link)
-                    .then((response) => {
-                        results.push({
-                            url: link,
-                            status: response.status,
-                            statusText: response.statusText,
-                        });
-                        console.log(`Valid: ${link}`);
-                    })
-                    .catch((error) => {
-                        results.push({
-                            url: link,
-                            error: error.message,
-                        });
-                        console.log(`Not valid: ${link}`);
-                    });
-            });
-            rl.on('close', () => {
-            });
-        } 
-    });
-}
 program.parse(process.argv);
-
--- src/index.js
+++ src/index.js
@@ -1,6 +1,50 @@
-export function sayHello() {
-  console.log("Olá, mundo!");
+const fs = require('fs');
+
+function readFile(fileName) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(fileName, 'utf8', (err, fileContent) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      const linkRegex = /https?:\/\/\S+/g;
+      const links = fileContent.match(linkRegex);
+      const results = [];
+
+      if (links) {
+        Promise.all(links.map((link) => {
+          return fetch(link)
+            .then((response) => {
+              results.push({
+                url: link,
+                status: response.status,
+                statusText: response.statusText,
+              });
+              return {
+                url: link,
+                status: response.status,
+                statusText: response.statusText,
+              };
+            })
+            .catch((error) => {
+              results.push({
+                url: link,
+                error: error.message,
+              });
+              return {
+                url: link,
+                error: error.message,
+              };
+            });
+        })).then(() => {
+          resolve(results);
+        });
+      } else {
+        resolve(results);
+      }
+    });
+  });
 }
 
-// Chamando a função
-sayHello();
+module.exports = readFile;
