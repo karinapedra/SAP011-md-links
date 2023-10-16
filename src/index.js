61d77d6fa99b16b86c54d54e021d2a840f450cd1
2karina <karina_kap@hotmail.com>
2023-10-16 12:02:43 -0300

    T004 - feat: completed the validations using options and insert chalk

--- src/index.js
+++ src/index.js
@@ -1,50 +1,84 @@
+// index.js
 const fs = require('fs');
+const path = require('path');
+const http = require('http');
 
-function readFile(fileName) {
+function extractLinks(markdownContent, file) {
+  const linkRegex = /(?:^|[^!])\[(.*?)\]\((https?:\/\/[^\s)]+)\)/g;
+  const links = [];
+
+  let match;
+  while ((match = linkRegex.exec(markdownContent)) !== null) {
+    const text = match[1];
+    const href = match[2];
+    links.push({ text, href, file });
+  }
+  return links;
+}
+
+
+function validateLink(link) {
   return new Promise((resolve, reject) => {
-    fs.readFile(fileName, 'utf8', (err, fileContent) => {
+    const url = new URL(link.href);
+    const options = {
+      host: url.host,
+      path: url.pathname,
+      method: 'HEAD',
+    };
+
+    const req = http.request(options, (res) => {
+      link.status = res.statusCode;
+      link.ok = res.statusCode >= 200 && res.statusCode < 400;
+      resolve(link);
+    });
+
+    req.on('error', () => {
+      link.status = null;
+      link.ok = false;
+      resolve(link);
+    });
+
+    req.end();
+  });
+}
+
+function mdLinks(filePath, options = {}) {
+  return new Promise((resolve, reject) => {
+    const absolutePath = path.resolve(filePath);
+
+    fs.readFile(absolutePath, 'utf8', (err, fileContent) => {
       if (err) {
         reject(err);
         return;
       }
 
-      const linkRegex = /https?:\/\/\S+/g;
-      const links = fileContent.match(linkRegex);
-      const results = [];
+      const links = extractLinks(fileContent, absolutePath);
 
-      if (links) {
-        Promise.all(links.map((link) => {
-          return fetch(link)
-            .then((response) => {
-              results.push({
-                url: link,
-                status: response.status,
-                statusText: response.statusText,
-              });
-              return {
-                url: link,
-                status: response.status,
-                statusText: response.statusText,
+      if (options.validate || options.stats) {
+        const linksPromises = links.map((link) => validateLink(link));
+
+        Promise.all(linksPromises)
+          .then((validatedLinks) => {
+            if (options.stats) {
+              const stats = {
+                total: validatedLinks.length,
+                unique: new Set(validatedLinks.map((link) => link.href)).size,
+                broken: validatedLinks.filter((link) => !link.ok).length,
               };
-            })
-            .catch((error) => {
-              results.push({
-                url: link,
-                error: error.message,
-              });
-              return {
-                url: link,
-                error: error.message,
-              };
-            });
-        })).then(() => {
-          resolve(results);
-        });
+              resolve({ links: validatedLinks, stats });
+            } else {
+              resolve({ links: validatedLinks });
+            }
+          })
+          .catch((error) => {
+            reject(error);
+          });
       } else {
-        resolve(results);
+        resolve({ links });
       }
     });
   });
 }
 
-module.exports = readFile;
+
+module.exports = mdLinks;
